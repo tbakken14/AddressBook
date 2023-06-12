d08e3468b26d27e4cf57034c7886178f3fd03d7b
Thomas Bakken <tbakken14@gmail.com>
2023-06-12 10:35:00 -0700

    Initial commit
    Co-authored-by: Shawn Cartwright <ponderosaprintingus@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function Contact(firstName = "", lastName = "", streetAddress = "", city = "", state = "",
+    state = "", territory = "", country = "", email = "", phone = "") {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.streetAddress = streetAddress;
+    this.city = city;
+    this.state = state;
+    this.territory = territory;
+    this.country = country;
+    this.email = email;
+    this.phone = phone;
+}
